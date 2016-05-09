16d81ccd8dbcaf335bf4ece712ccc94d70c515e0
Rick Mottola <mottola.rick@gmail.com>
2016-05-09 01:10:50 +0000

    initial commit with sign up capability

---
+++ app.js
@@ -0,0 +1,63 @@
+var express = require("express");
+var app = express();
+var mongoose = require("mongoose");
+var passport = require("passport");
+var bodyParser = require("body-parser");
+var LocalStrategy = require("passport-local");
+var passportLocalMongoose = require("passport-local-mongoose");
+var User = require("./models/user");
+
+mongoose.connect('mongodb://localhost/auth_demo_app');
+
+app.use(require("express-session")({
+    secret: 'Sephie is the best and cutest dog in the world',
+    resave: false,
+    saveUninitialized:false
+}));
+
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// ROUTES \\
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+
+app.get('/secret', function(req, res) {
+    res.render('secret');
+});
+
+// Auth Routes \\
+
+// show sign up form
+app.get('/register', function(req, res) {
+    res.render('register');
+});
+
+// handle user sign up
+app.post('/register', function(req, res) {
+    req.body.username;
+    req.body.password;
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user) {
+        if(err) {
+            console.log(err);
+            return res.render('register');
+        } 
+        // authenticate user and login
+        passport.authenticate('local')(req, res, function() {
+            res.redirect('/secret');
+        });
+    });
+});
+
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log('Server Up!');
+});
