59d1bee12a8739ce40ac6e0a614fa6e8a9d3bc0e
Rick Mottola <mottola.rick@gmail.com>
2016-05-09 01:44:55 +0000

    finalized authentications - app complete!

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.get('/', function(req, res) {
     res.render('home');
 });
 
-app.get('/secret', function(req, res) {
+app.get('/secret', isLoggedIn, function(req, res) {
     res.render('secret');
 });
 
@@ -70,6 +70,17 @@ app.post('/login', passport.authenticate('local', {
     
 });
 
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/login');
+}
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log('Server Up!');
